2d7bd9f669b2414d6053fca3d4e6e51b4cbb2d3f
Ivan Morozov <m0r0zk01@yandex.ru>
2021-10-04 13:17:18 +0300

    added filename ctor

--- writer/writer.h
+++ writer/writer.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <fstream>
 #include <istream>
 #include <vector>
 
@@ -7,14 +8,19 @@ class Writer {
 public:
     Writer();
     Writer(std::ostream& os);
+    Writer(std::string_view filename);
+
+    ~Writer();
 
     void WriteBit(bool bit);
     void WriteBits(const std::vector<bool>& bits);
+    void WriteNBits(size_t bits, size_t amount);
     void End();
     void SetOutputStream(std::ostream& os);
 
 private:
     std::ostream* output_stream_;
+    bool has_stream_ownership_;
     size_t bits_left_;
     unsigned char last_byte_;
 };
