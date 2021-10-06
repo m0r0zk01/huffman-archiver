23b83c8fa99efa67476a78dd54585e6ea7bd0bb6
Ivan Morozov <m0r0zk01@yandex.ru>
2021-10-06 23:54:05 +0300

    Compressor: splitted AddFile() func into several smaller funcs

--- archiver/compressor.h
+++ archiver/compressor.h
@@ -11,14 +11,16 @@ public:
     void EndArchive();
 
 private:
-    size_t files_added_ = 0;
-
-    using CodeTable = std::unordered_map<size_t, std::pair<size_t, size_t>>;
-    CodeTable code_table_;
-
     void EncodeFileName();
     void MakeCanonicalHuffmanCode(const std::vector<std::pair<size_t, size_t>>& codes,
                                   std::unordered_map<size_t, size_t>& cnt_len_code);
     void WriteCodeTableToFile(size_t max_symbol_code_size,
                               std::unordered_map<size_t, size_t>& cnt_len_code);
+    Trie BuildTrie(std::unordered_map<size_t, size_t>& cnt_bytes);
+    std::unordered_map<size_t, size_t> BuildBytesFrequencyMap();
+
+    size_t files_added_ = 0;
+
+    using CodeTable = std::unordered_map<size_t, std::pair<size_t, size_t>>;
+    CodeTable code_table_;
 };
