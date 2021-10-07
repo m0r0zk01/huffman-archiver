f9a09b314123898952c67181d49d638c52fb9c6a
Ivan Morozov <m0r0zk01@yandex.ru>
2021-10-07 11:35:45 +0300

    fixed tests for archiver and CMakeLists for all modules

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,33 +1,77 @@
 #include "../archiver/compressor.h"
 #include "../archiver/decompressor.h"
 
+#include <cstdio>
 #include <iostream>
 
 #include <gtest/gtest.h>
 
 TEST(TestArchvier, ArchiveSingleFile) {
-    Reader reader("./mock/sample/amogus.txt");
-    Writer writer("./arch");
-    Compressor compressor(&writer);
-    compressor.AddFile(&reader);
-    compressor.EndArchive();
-    reader.SetInputStream("./arch");
-    std::cout << "HERE\n";
-    Decompressor decompressor(&reader);
-    std::cout << "HERE\n";
-    decompressor.Decompress();
-
-    std::ifstream file1("./mock/sample/amogus.txt");
-    std::ifstream file2("./amogus.txt");
-
-    char c1, c2;
-    while (file1.get(c1)) {
-        ASSERT_TRUE(file2.get(c2));
-        ASSERT_EQ(c1, c2);
+    {
+        Reader reader("./mock/sample/watermelon.mp3");
+        Writer writer("./mock/arch");
+        Compressor compressor(&writer);
+        compressor.AddFile(&reader);
+        compressor.EndArchive();
     }
-    ASSERT_TRUE(file2.get(c2));
-    ASSERT_TRUE(file1.bad());
-    ASSERT_TRUE(file2.bad());
+    {
+        Reader reader("./mock/arch");
+        Decompressor decompressor(&reader);
+        decompressor.Decompress();
+    }
+    {
+        std::ifstream file1("./mock/sample/watermelon.mp3");
+        std::ifstream file2("./watermelon.mp3");
+
+        char c1, c2;
+        while (file1.get(c1)) {
+            ASSERT_TRUE(file2.get(c2));
+            ASSERT_EQ(c1, c2);
+        }
+        ASSERT_FALSE(file2.get(c2));
+        ASSERT_TRUE(file1.eof());
+        ASSERT_TRUE(file2.eof());
+        std::remove("watermelon.mp3");
+    }
+}
+
+TEST(TestArchvier, ArchiveMultipleFiles) {
+    std::vector<std::string> files {"amogus.txt", "test.pdf", "a", "asd.png"};
+    {
+        Writer writer("./mock/arch");
+        Compressor compressor(&writer);
+
+        Reader reader;
+        for (const auto& filename : files) {
+            reader.SetInputStream("./mock/sample/" + filename);
+            compressor.AddFile(&reader);
+        }
+
+        compressor.EndArchive();
+    }
+    {
+        Reader reader("./mock/arch");
+        Decompressor decompressor(&reader);
+        decompressor.Decompress();
+    }
+    {
+        for (const auto& filename : files) {
+            std::cout << filename << '\n';
+            std::ifstream file1("./mock/sample/" + filename);
+            std::ifstream file2("./" + filename);
+
+            char c1, c2;
+            while (file1.get(c1)) {
+                ASSERT_TRUE(file2.get(c2));
+                ASSERT_EQ(c1, c2);
+            }
+            ASSERT_FALSE(file2.get(c2));
+            ASSERT_TRUE(file1.eof());
+            ASSERT_TRUE(file2.eof());
+
+            std::remove(filename.data());
+        }
+    }
 }
 
 int main(int argc, char** argv) {
