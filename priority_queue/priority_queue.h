12143a1ece97dec3f95ccef73505ffa9104a8255
Ivan Morozov <m0r0zk01@yandex.ru>
2021-10-09 02:16:15 +0300

    changed readme.md, changed help language to English, build.sh now can build in debug mode if --debug is used (default is release), basic benchmarks sh script

--- priority_queue/priority_queue.h
+++ priority_queue/priority_queue.h
@@ -5,7 +5,7 @@
 template <typename T>
 class PriorityQueue {
 public:
-    PriorityQueue() {}
+    PriorityQueue() = default;
     explicit PriorityQueue(const std::vector<T>& elements) {
         for (const auto& element : elements) {
             Insert(element);
